2d0dde4ac59c7ca1d0578d7a627212b4fce94675
neroist <pmpiga1@gmail.com>
2024-02-29 21:30:14 -0500

    form 2 windows

--- data/graph.py
+++ data/graph.py
@@ -8,20 +8,31 @@ with open("./data.json", "r") as file:
     data = json.load(file)
 
 depths = [i for i in range(1, 9)]
-
 time = [data[i]["time_taken"] for i in range(0, 8)]
 lose_rates = [data[i]["loss_rate"] for i in range(0, 8)]
 
-sns.set_theme("poster", "darkgrid", palette="pastel")
+title_size = 40
+label_size = 32
 
-plt.title("Minimax Algorithm vs. Random Moves", fontsize=18)
-# plt.ylim(0, 375)
-plt.xlabel('AI Max Depth', fontsize=16)
-plt.ylabel('Lose Rate %', fontsize=16)
+sns.set_theme("poster", "darkgrid", palette="pastel")
 
 # palettes:
 # autumn, cool, hot?, hsv, plasma, spring, vidris, winter,
 # husl, hls
 
-sns.barplot(x=depths, y=lose_rates, palette="spring")
+# 1st figure, depth to time
+plt.figure(1)
+plt.title("Minimax Algorithm vs. Random Moves", fontsize=title_size)
+# plt.ylim(0, 375)
+plt.xlabel('AI Max Depth', fontsize=label_size)
+plt.ylabel('Time Taken (in seconds)', fontsize=label_size)
+sns.barplot(x=depths, y=time, palette="cool")
+
+# 2nd figure, depth to lose rate
+plt.figure(2)
+plt.title("Minimax Algorithm vs. Random Moves", fontsize=title_size)
+plt.xlabel('AI Max Depth', fontsize=label_size)
+plt.ylabel('Lose Rate %', fontsize=label_size)
+sns.barplot(x=depths, y=lose_rates, palette="winter")
+
 plt.show()
